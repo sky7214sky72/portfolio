fb4c5b71f12dcf69c5830e4c00eaed8b5a1ba49b
wonseok.choi <sky7214sky72@naver.com>
2024-06-28 17:21:02 +0900

    코드 정리

--- src/main/java/org/example/portfolio/PortfolioApplication.java
+++ src/main/java/org/example/portfolio/PortfolioApplication.java
@@ -1,5 +1,11 @@
 package org.example.portfolio;
 
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -11,7 +17,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PortfolioApplication {
 
   public static void main(String[] args) {
+    final Logger logger = LoggerFactory.getLogger(PortfolioApplication.class);
     SpringApplication.run(PortfolioApplication.class, args);
+    logger.info(
+        "현재 시간 " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
   }
 
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
 }
