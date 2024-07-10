c3392b366602132dd17c688ac8c5a7274c4bd986
wonseok.choi <sky7214sky72@naver.com>
2024-07-10 23:46:23 +0900

    no static error 수정

--- src/main/java/org/example/portfolio/global/config/WebConfig.java
+++ src/main/java/org/example/portfolio/global/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:8080", "https://kauth.kakao.com/oauth/authorize",
-            "https://kauth.kakao.com/oauth/authorize", "https://kapi.kakao.com/v2/user/me", "http://localhost:3000")
+            "https://kauth.kakao.com/oauth/authorize", "https://kapi.kakao.com/v2/user/me", "http://localhost:4000")
         .allowedMethods(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
